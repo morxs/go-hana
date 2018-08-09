a4e462b5a02e4d4a9a3a854e7a949a0b09e0ba40
rudsu <free4spams@yahoo.com>
2018-08-09 14:01:21 +0700

    - add batch for repeatable build

---
+++ build_prg.bat
@@ -0,0 +1,15 @@
+echo "Build main program"
+go build -ldflags "-s -w" ekko.go 
+go build -ldflags "-s -w" ekpo.go
+go build -ldflags "-s -w" lfa1.go
+go build -ldflags "-s -w" mara.go
+go build -ldflags "-s -w" t024.go
+go build -ldflags "-s -w" t024e.go
+go build -ldflags "-s -w" tcurr.go
+go build -ldflags "-s -w" zstxl.go
+
+echo "Build other utilities"
+go build -ldflags "-s -w" dl_consolpack_rtemplate.go
+go build -ldflags "-s -w" dl_gl_consol_pack_map.go
+go build -ldflags "-s -w" gen_code_ddf.go
+go build -ldflags "-s -w" upload_gl_consol_pack_map.go
