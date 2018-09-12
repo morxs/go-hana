ea2b42810ace8abb5170ed4f7be7f53fcb69c3c4
rudsu <free4spams@yahoo.com>
2018-09-12 11:34:02 +0700

    add echo off

--- upx_go-hana.bat
+++ upx_go-hana.bat
@@ -1,1 +1,3 @@
-upx --brute lfa1.exe ekko.exe ekpo.exe t024.exe t024e.exe tcurr.exe zstxl.exe mara.exe 
+@echo off
+echo "Compressing binary using UPX"
+upx -k --brute lfa1.exe ekko.exe ekpo.exe t024.exe t024e.exe tcurr.exe zstxl.exe mara.exe 
