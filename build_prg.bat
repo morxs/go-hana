1a88f613c689b60ce554ba90c55f9d22c769d9e9
rudsu <free4spams@yahoo.com>
2019-05-13 16:52:01 +0700

    updated build path

--- build_prg.bat
+++ build_prg.bat
@@ -1,16 +1,15 @@
-@echo off
 echo "Build main program"
-go build -ldflags "-s -w" ekko.go 
-go build -ldflags "-s -w" ekpo.go
-go build -ldflags "-s -w" lfa1.go
-go build -ldflags "-s -w" mara.go
-go build -ldflags "-s -w" t024.go
-go build -ldflags "-s -w" t024e.go
-go build -ldflags "-s -w" tcurr.go
-go build -ldflags "-s -w" zstxl.go
+go build -ldflags "-s -w" cmd\ekko\ekko.go 
+go build -ldflags "-s -w" cmd\ekpo\ekpo.go
+go build -ldflags "-s -w" cmd\lfa1\lfa1.go
+go build -ldflags "-s -w" cmd\mara\mara.go
+go build -ldflags "-s -w" cmd\t024\t024.go
+go build -ldflags "-s -w" cmd\t024e\t024e.go
+go build -ldflags "-s -w" cmd\tcurr\tcurr.go
+go build -ldflags "-s -w" cmd\zstxl\zstxl.go
 
 echo "Build other utilities"
-go build -ldflags "-s -w" dl_consolpack_rtemplate.go
-go build -ldflags "-s -w" dl_gl_consol_pack_map.go
-go build -ldflags "-s -w" gen_code_ddf.go
-go build -ldflags "-s -w" upload_gl_consol_pack_map.go
+go build -ldflags "-s -w" cmd\dl_consolpack_rtemplate\dl_consolpack_rtemplate.go
+go build -ldflags "-s -w" cmd\dl_gl_consol_pack_map\dl_gl_consol_pack_map.go
+go build -ldflags "-s -w" cmd\gen_code_ddf\gen_code_ddf.go
+go build -ldflags "-s -w" cmd\upload_gl_consol_pack_map\upload_gl_consol_pack_map.go
