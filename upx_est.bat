b7efe83051d80336aa8224aa5ca9eb46d4666cc8
rudsu <free4spams@yahoo.com>
2019-05-13 17:59:30 +0700

    compress new zest binaries

---
+++ upx_est.bat
@@ -0,0 +1,4 @@
+@echo off
+
+echo "Compressing other binary using UPX"
+exes\upx.exe -k --brute zest_rday.exe zest_oil_pom.exe zest_estate.exe zest_division.exe zest_blockh.exe zest_blockb.exe zest_block2.exe zest_block.exe
