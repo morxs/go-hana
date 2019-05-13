288c8b7cd9e9243224d4d1291447991d03dfa7e6
rudsu <free4spams@yahoo.com>
2019-05-13 17:59:17 +0700

    add compression to other tools as well

--- upx_go-hana.bat
+++ upx_go-hana.bat
@@ -1,3 +1,6 @@
 @echo off
 echo "Compressing binary using UPX"
-upx -k --brute lfa1.exe ekko.exe ekpo.exe t024.exe t024e.exe tcurr.exe zstxl.exe mara.exe 
+exes\upx.exe -k --brute lfa1.exe ekko.exe ekpo.exe t024.exe t024e.exe tcurr.exe zstxl.exe mara.exe 
+
+echo "Compressing utilities using UPX"
+exes\upx.exe -k --brute upload_gl_consol_pack_map.exe gen_code_ddf.exe dl_gl_consol_pack_map.exe dl_consolpack_rtemplate.exe
